d47e48b6f00ff70e971e8a8a61a92f7560f8e174
hrsqs <510233528@qq.com>
2018-01-09 09:34:49 +0800

    添加注释

--- CJUtilWork/Data/CJUtil.h
+++ CJUtilWork/Data/CJUtil.h
@@ -10,13 +10,13 @@
 #import "CJMacro.h"
 
 // 基本类分类定义
-#import "NSString+CJString.h"
+#import "NSString+CJString.h"           // 包括NSString、NSAttributedString、NSMutableString分类
 #import "UIView+CJView.h"
 #import "UIImage+CJImage.h"
 #import "UIButton+CJButton.h"
 #import "NSObject+CJObject.h"
-#import "NSArray+CJArray.h"
-#import "NSDictionary+CJDictionary.h"
+#import "NSArray+CJArray.h"             // 包括NSArray、NSMutableArray分类
+#import "NSDictionary+CJDictionary.h"   // 包括NSDictionary、NSMutableDictionary分类
 
 
-#define CJUTIL_VERSION @"1.0.0"
+#define CJUTIL_VERSION @"1.0.1"
